92ac2a782c8d2bed2b0532dc666dd4e7086b0589
Samuel Theofie <samueltheofie@gmail.com>
2026-02-28 20:54:11 +0000

    Init of mycfg in Rust

---
+++ cfg/src/main.rs
@@ -0,0 +1,43 @@
+use std::{
+    fs,
+    io::{Read, stdin},
+    path::PathBuf,
+};
+
+use argh::FromArgs;
+use bril_rs::load_program_from_read;
+use snafu::{ResultExt, Whatever};
+
+#[derive(FromArgs)]
+/// Generates a CFG from Bril's canonical JSON representation
+#[argh(help_triggers("-h", "--help", "help"))]
+struct Opts {
+    /// input Bril file: omit for stdin
+    #[argh(positional)]
+    input_path: Option<PathBuf>,
+}
+
+#[snafu::report]
+fn main() -> Result<(), Whatever> {
+    let opts = argh::from_env::<Opts>();
+
+    let (input_path_string, mut reader): (String, Box<dyn Read>) = {
+        if let Some(input_path) = opts.input_path {
+            let input_path_string = input_path.to_string_lossy().to_string();
+            (
+                input_path_string.clone(),
+                Box::new(
+                    fs::File::open(&input_path)
+                        .whatever_context(format!("Failed to open file: {}", input_path_string))?,
+                ),
+            )
+        } else {
+            ("<stdin".to_owned(), Box::new(stdin()))
+        }
+    };
+
+    let program = load_program_from_read(reader);
+
+    println!("{}", program);
+    Ok(())
+}
---
+++ cfg/src/util/cfg.rs
@@ -0,0 +1,7 @@
+use bril_rs::Program;
+
+pub fn get_cfg(program: &Program) {
+    for function in &program.functions {
+        todo!("Loop over function instructions here");
+    }
+}
