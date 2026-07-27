5c3c341752563922893baca8b69a1333f1b548f1
Samuel Theofie <samueltheofie@gmail.com>
2026-07-27 14:36:34 +0200

    feat: util structs implemented

--- bril-project/src/types/mod.rs
+++ bril-project/src/types/mod.rs
@@ -1,3 +1,5 @@
 mod cfg_types;
+mod util_types;
 
 pub use cfg_types::{BasicBlock, BlockId, FunctionCFG, Label, ProgramCFG};
+pub use util_types::BitSet;
---
+++ bril-project/src/types/util_types/bitset.rs
@@ -0,0 +1,43 @@
+/// Bitset struct
+pub struct BitSet {
+    words: Vec<u64>,
+}
+
+impl BitSet {
+    pub fn new_all_true(size: usize) -> Self {
+        let num_words = (size + 63) / 64;
+        Self {
+            words: vec![!0u64; num_words], // !0u64 sets all 64 bits to 1
+        }
+    }
+
+    pub fn new_all_false(size: usize) -> Self {
+        let num_words = (size + 63) / 64;
+        Self {
+            words: vec![0u64; num_words],
+        }
+    }
+
+    /// Mark an index as `false` (dead)
+    pub fn mark_dead(&mut self, idx: usize) {
+        self.words[idx / 64] &= !(1u64 << (idx % 64));
+    }
+
+    /// Mark an index as `true` (alive)
+    pub fn mark_keep(&mut self, idx: usize) {
+        self.words[idx / 64] |= 1u64 << (idx % 64);
+    }
+
+    /// Read whether index is `true` (keep)
+    pub fn is_keep(&self, idx: usize) -> bool {
+        (self.words[idx / 64] & (1u64 << (idx % 64))) != 0
+    }
+
+    pub fn clear_true(&mut self) {
+        self.words.fill(!0u64);
+    }
+
+    pub fn clear_false(&mut self) {
+        self.words.fill(0u64);
+    }
+}
